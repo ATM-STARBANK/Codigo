03f0497eab2a928ccf128e6129ce5f75e6bcd575
LlanesGood <157669664+LlanesGood@users.noreply.github.com>
2024-03-02 19:43:27 -0600

    Create Menuservicios.cs

---
+++ ATMStarbank/Menuservicios.cs
@@ -0,0 +1,51 @@
+﻿namespace ATMStarbank;
+
+class MenuServicios
+{
+    static void Main(string[] args)
+    {
+        string Opciones;
+
+        Console.WriteLine("Pago de Servicios");
+        Console.WriteLine("Elija la operacion que desea hacer");
+        Console.WriteLine("1.CFE");
+        Console.WriteLine("2.TELMEX");
+        Console.WriteLine("3.JAPAY");
+        Console.WriteLine("4.SPOTIFY");
+        Console.WriteLine("5.NETFLIX");
+        Console.WriteLine("6.Regresar");
+            Opciones = Console.ReadLine();
+            
+                switch (Opciones)
+                {
+                    case "1":
+                    
+                        
+                        break;
+                    
+                    case "2":
+                    
+                        
+                        break;
+                    
+                    case "3":
+                    
+                        
+                        break;
+                    
+                    case "4":
+                    
+                        
+                        break;
+                    
+                    case "5": 
+
+                        break;
+
+                }
+                if (Opciones =="6")
+                {
+                Console.WriteLine("");
+                } 
+     }
+}
