a95f4309bef56cb3d1cae02963256153bdcc15ef
LlanesGood <157669664+LlanesGood@users.noreply.github.com>
2024-03-02 19:04:16 -0600

    Update Menucredito.cs

---
+++ ATMStarbank/Menucredito.cs
@@ -0,0 +1,32 @@
+namesapace 
+class Menucredito
+{
+    static void Main(string[] args)
+    {
+        string Opciones;
+
+        Console.WriteLine("Trasacciones con Targera de Credito");
+        Console.WriteLine("Elija la operacion que desea hacer");
+        Console.WriteLine("1.Pago con Targeta");
+        Console.WriteLine("2.Pago credito");
+        Console.WriteLine("3.Regresar");
+            Opciones = Console.ReadLine();
+            
+                switch (Opciones)
+                {
+                    case "1":
+                    
+                        
+                        break;
+                    
+                    case "2":
+                    
+                        
+                        break;
+                }
+                if (Opciones =="3")
+                {
+                Console.WriteLine("Gracias por usar el program");
+                } 
+     }
+}
