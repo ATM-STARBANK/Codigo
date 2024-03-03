ee4d95e278b3c0b1a59ab4f92a02dc100ec5ab7d
LlanesGood <157669664+LlanesGood@users.noreply.github.com>
2024-03-02 18:17:37 -0600

    Update Menudebito.cs

---
+++ ATMStarbank/Menudebito.cs
@@ -0,0 +1,58 @@
+namespace Banco;
+
+class Menu
+{
+    static void Main(string[] args)
+    {
+
+        string Opciones;
+
+        Console.WriteLine("Bienvenidos al Banco");
+        Console.WriteLine("       StarBank    ");
+        Console.WriteLine("                   ");
+        Console.WriteLine("1.Disponidad de efectivo");
+        Console.WriteLine("2.Transacciones");
+        Console.WriteLine("3.Retiro Efectivo");
+        Console.WriteLine("4.Depositar en Efectivo");
+        Console.WriteLine("5.Consultar Saldo");
+        Console.WriteLine("6.Salir");
+
+        Opciones = Console.ReadLine();
+            
+                switch (Opciones)
+                {
+                    case "1":
+                    
+                        
+                        break;
+                    
+                    case "2":
+                    
+                        
+                        break;
+                    
+                    case "3":
+                    
+                        
+                        break;
+                    
+                    case "4":
+                    
+                        
+                        break;
+                    
+                    case "5": 
+
+                        break;
+
+                }
+                if (Opciones =="6")
+                {
+                Console.WriteLine("Gracias por usar el program");
+                break;
+                }
+                    
+             
+        }
+
+    }
