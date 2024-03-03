203db5e007c326172d409115ea728dbad5d8974d
Spartbrine <108151498+Spartbrine@users.noreply.github.com>
2024-03-02 19:48:30 -0600

    Ortografia debito

--- ATMStarbank/Menudebito.cs
+++ ATMStarbank/Menudebito.cs
@@ -6,14 +6,14 @@ class Menudebito
     {
         string Opciones;
 
-        Console.WriteLine("Trasacciones con Targera de Debito");
-        Console.WriteLine("Elija la operacion que desea hacer");
-        Console.WriteLine("1.Disponidad de efectivo");
-        Console.WriteLine("2.Transacciones");
+        Console.WriteLine("Trasacciones con Tarjeta de Debito");
+        Console.WriteLine("Elija la operacion que desea realizar");
+        Console.WriteLine("1.Disponibilidad de efectivo"); //Eliminalo
+        Console.WriteLine("2.Transacciones"); //eliminalo
         Console.WriteLine("3.Retiro Efectivo");
         Console.WriteLine("4.Depositar en Efectivo");
         Console.WriteLine("5.Consultar Saldo");
-        Console,WriteLine("6.Registrar un nuevo ping");
+        Console,WriteLine("6.Registrar un nuevo PIN"); 
         Console.WriteLine("7.Pago de Servicios");
         Console.WriteLine("8.Regresar");
             Opciones = Console.ReadLine();
