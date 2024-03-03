c552312d6c4c9b39997855da44547ddbd732bfc7
LlanesGood <157669664+LlanesGood@users.noreply.github.com>
2024-03-02 19:38:36 -0600

    Update Program.cs

--- ATMStarbank/Program.cs
+++ ATMStarbank/Program.cs
@@ -1,24 +1,4 @@
 ﻿namespace ATMStarbank;
 
 class Program
-{
-    static void Main(string[] args)
-    {
-        string contra;
 
-        Console.WriteLine("Bienvenidos al Banco");
-        Console.WriteLine("       StarBank    ");
-        Console.WriteLine("                   ");
-        Console.WriteLine("Inicie sesion con su contraseña");
-        contra=Console.ReadLine();
-
-        if(contra =  )
-        {
-            
-        }
-        else
-        {
-            Console.WriteLine("Cerrando sesion ...");
-        }
-    }
-}
